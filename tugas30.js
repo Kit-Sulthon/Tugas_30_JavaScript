8774beeffc27696117b6b08d412284159a6c4691
T R A $ H <sulthons.a19@gmail.com>
2023-10-06 08:55:27 +0700

    Tugas 30 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas30.js
@@ -0,0 +1,5 @@
+function tuggas30(value) {
+  var datatugas2 = value.match(/[es]/g);
+  console.log(datatugas2);
+}
+tuggas30("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
